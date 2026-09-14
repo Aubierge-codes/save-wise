79c8bee7a756f074df1bdae04b9776b5c3affdd9
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-14 20:16:18 +0200

    checkpoint 01: initialize SaveWise

---
+++ Main.java
@@ -0,0 +1,14 @@
+package src;
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("            SAVEWISE");
+        System.out.println("     Personal Money Assistant");
+        System.out.println("=================================");
+
+        System.out.println("Welcome to SaveWise!");
+        System.out.println("Let's help you manage your money.");
+    }
+}
