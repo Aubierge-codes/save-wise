764de1ed14b32cd2a1c95c645436594ef6d4a6f1
Umurerwa Aubierge <aubierge7557@gmail.com>
2026-09-14 20:20:17 +0200

    checkpoint 03: create Expense class

---
+++ Expense.java
@@ -0,0 +1,24 @@
+public class Expense {
+
+    private String description;
+    private String category;
+    private double amount;
+
+    public Expense(String description, String category, double amount) {
+        this.description = description;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
--- Main.java
+++ Main.java
@@ -4,12 +4,28 @@ public class Main {
 
         Income income = new Income("Allowance", 50000);
 
+        Expense expense = new Expense(
+                "Lunch",
+                "Food",
+                5000
+        );
+
         System.out.println("=================================");
         System.out.println("            SAVEWISE");
         System.out.println("     Personal Money Assistant");
         System.out.println("=================================");
 
-        System.out.println("Income source: " + income.getSource());
-        System.out.println("Income amount: " + income.getAmount() + " RWF");
+        System.out.println();
+
+        System.out.println("INCOME");
+        System.out.println("Source: " + income.getSource());
+        System.out.println("Amount: " + income.getAmount() + " RWF");
+
+        System.out.println();
+
+        System.out.println("EXPENSE");
+        System.out.println("Description: " + expense.getDescription());
+        System.out.println("Category: " + expense.getCategory());
+        System.out.println("Amount: " + expense.getAmount() + " RWF");
     }
 }
